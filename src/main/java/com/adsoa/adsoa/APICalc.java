893f0221b2a36a30df7cd4784311b6800686d5fc
Alberto L. Figueroa <0234621@up.mx>
2023-08-30 22:23:30 -0600

    Yes

--- src/main/java/com/adsoa/adsoa/APICalc.java
+++ src/main/java/com/adsoa/adsoa/APICalc.java
@@ -5,13 +5,15 @@ import static spark.Spark.get;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import org.json.JSONObject;
 
 public class APICalc {
     public static void main(String[] args) {
         get("/hello", new Route() {
             @Override
             public Object handle(Request request, Response response) throws Exception {
-                return "Hello World!";
+                response.type("application/json");
+                return new JSONObject().put("message", "Hola ADSOA!").toString();
             }
         });
 
@@ -24,22 +26,15 @@ public class APICalc {
                 double operand1 = Double.parseDouble(request.queryParams("operand1"));
                 double operand2 = Double.parseDouble(request.queryParams("operand2"));
                 System.out.println(request.queryParams());
-                double result = 0;
-                switch (operator) {
-                    case "+":
-                        result = operand1 + operand2;
-                        break;
-                    case "-":
-                        result = operand1 - operand2;
-                        break;
-                    case "*":
-                        result = operand1 * operand2;
-                        break;
-                    case "/":
-                        result = operand1 / operand2;
-                        break;
-                }
-                return "{\"result\": " + result + "}";
+                double result = switch (operator) {
+                    case "+" -> operand1 + operand2;
+                    case "-" -> operand1 - operand2;
+                    case "*" -> operand1 * operand2;
+                    case "/" -> operand1 / operand2;
+                    default -> 0;
+                };
+                response.type("application/json");
+                return new JSONObject().put("result", result).toString();
             }
         });
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.adsoa.adsoa {
     requires org.kordamp.bootstrapfx.core;
     requires spark.core;
     requires java.net.http;
+    requires org.json;
 
     opens com.adsoa.adsoa to javafx.fxml;
     exports com.adsoa.adsoa;
